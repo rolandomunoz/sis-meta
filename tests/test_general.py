1894167644a6f3c714293d502e2be1b81670209e
rolandomunoz <rolando.muar@gmail.com>
2023-03-19 21:27:23 -0500

    add tests

---
+++ tests/test_general.py
@@ -0,0 +1,14 @@
+import sys
+from pathlib import Path
+sis_meta_path = str(Path(__file__).parent.parent.joinpath('src'))
+sys.path.insert(0, sis_meta_path)
+import sis_meta
+
+data_dir = Path(__file__).parent / 'data'
+for meta_path in data_dir.glob('*.meta'):
+    meta = sis_meta.Meta(meta_path)
+
+    print(meta_path.name)
+    for mark in meta:
+        print(mark)
+    print()
