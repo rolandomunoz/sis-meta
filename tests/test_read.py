0dbd8b33992f2010e15b85cbafbe6a49b562eaf1
rolandomunoz <rolando.muar@gmail.com>
2023-03-25 16:28:33 -0500

    edit: add comment

--- tests/test_read.py
+++ tests/test_read.py
@@ -1,3 +1,6 @@
+"""
+Read meta files.
+"""
 import sys
 from pathlib import Path
 sis_meta_path = str(Path(__file__).parent.parent.joinpath('src'))
@@ -6,7 +9,7 @@ import sis_meta
 
 data_dir = Path(__file__).parent / 'data'
 for meta_path in data_dir.glob('*.meta'):
-    meta = sis_meta.Meta(meta_path)
+    meta = sis_meta.read_from_file(meta_path)
 
     print(meta_path.name)
     for mark in meta:
