4dfbb9734210392f77cdc7622ac37748dea178ff
rolandomunoz <rolando.muar@gmail.com>
2023-03-25 16:31:12 -0500

    changed: meta only handle data. It does not read meta files anymore

--- src/sis_meta/meta.py
+++ src/sis_meta/meta.py
@@ -1,119 +1,62 @@
 """
 Handle annotation files (.meta).
-
-Tested on:
-    - SIS II (v2.6)
 """
-import re
-import struct
-
-POSITIONS = re.compile(rb'\nPOSITIONS; BEGIN; VECTOR_DOUBLE;BINARY;(\d+)\n(.+)\nPOSITIONS; END', re.DOTALL)
-IDS = re.compile(rb'\nIDS; BEGIN; VECTOR_INT;BINARY;(\d+)\n(.+)\nIDS; END', re.DOTALL)
-LENGTHS = re.compile(rb'\nLENGTHS; BEGIN; VECTOR_DOUBLE;BINARY;(\d+)\n(.+)\nLENGTHS; END', re.DOTALL)
-TEXTS_LONG = re.compile(rb'\nTEXTS; BEGIN; VECTOR_STRING\n(.+)\nTEXTS; END\n', re.DOTALL)
-TEXTS_SHORT = re.compile(rb'\nTEXTS; (.+)\n\nTEXT_ATTR_POSITIONS; BEGIN;', re.DOTALL)
-HEAD = re.compile(rb'\nID; (\d+?)\n\nNAME; (.+?)\n\n', re.DOTALL)
-
 class Meta:
     """
-    Class for data in meta files.
+    Class for handling guide marks.
     """
-    def __init__(self, path):
-        self.path = path
-        self.data = self._parse_file(path)
+    def __init__(self):
+        self.data = []
 
     def __iter__(self):
         return iter(self.data)
 
-    def _parse(self, content):
+    def insert_guide_mark(self, position, length = 0, text = '', group_id = 6):
         """
-        Parse a meta file format and get its marks.
+        Insert a guide mark.
+        """
+        guide_mark = GuideMark(position, length, text, group_id, 'gaaa')
+        self.data.append(guide_mark)
 
-        Parameters
-        ----------
-        content : bytes
-            The content of a meta file
+class GuideMark:
+    """
+    A guide mark.
+    """
+    def __init__(self, position, length, text, group_id, group_name):
+        self.position = position
+        self.length = length
+        self.text = text
+        self.group_id = group_id
+        self.group_name = group_name
+
+    def __repr__(self):
+        dict_ = {
+            'position': round(self.position, 2),
+            'length': round(self.length, 2),
+            'text': self.text,
+            'group_id': self.group_id,
+            'group_name': self.group_name,
+        }
+        return dict_.__repr__()
+
+    def is_interval(self):
+        """
+        Check if the guide mark is an interval.
 
         Returns
         -------
-        zip of tuples, [(position, length, text, group_id, group_name), ...]
-            A zip of tuples. Each tuple contains the start position,
-            length, text, group_id and group_name of a mark.
+        bool : True or False
+            `True` if the length is greater than 0. Otherwise, it returns `False`.
         """
-        # Get the group names
-        head_dict = {}
-        head = HEAD.findall(content)
-        for group_id, group_name in head:
-            head_dict[group_id.decode()] = group_name.decode()
+        return self.length != 0
 
-        # Read positions
-        positions_match = POSITIONS.search(content)
-        #position_size = positions_match.group(1)
-        positions_bytes = positions_match.group(2)[4:]
-        positions = struct.unpack(f'{len(positions_bytes) // 8}d', positions_bytes)
-
-        # Read ids / also known as groups
-        ids_match = IDS.search(content)
-        #ids_size = ids_match.group(1)
-        ids_bytes = ids_match.group(2)[4:]
-        group_ids = struct.unpack(f'{len(ids_bytes) // 4}i', ids_bytes)
-        group_names = [head_dict[str(group_id)] for group_id in group_ids]
-
-        # Read lengths
-        lengths_match = LENGTHS.search(content)
-        #lengths_size = lengths_match.group(1)
-        lengths_bytes = lengths_match.group(2)[4:]
-        lengths = struct.unpack(f'{len(lengths_bytes) // 8}d', lengths_bytes)
-
-        # Read texts
-        texts_match = TEXTS_LONG.search(content)
-        if texts_match is None:
-            texts_match = TEXTS_SHORT.search(content)
-        texts_bytes = texts_match.group(1)
-        texts = []
-        for text_bytes in texts_bytes.split(b';'):
-            text_bytes = text_bytes.replace(bytes.fromhex('02'), b'\n') #Start of Text: U+0002
-            text_bytes = text_bytes.replace(bytes.fromhex('03'), b';') #End of Text: U+0003
-            text_bytes = text_bytes.replace(b'_|!!|_nuse', b'') #End of Text: U+0003
-            texts.append(text_bytes.decode())
-        return zip(positions, lengths, texts, group_ids, group_names)
-
-    def _parse_file(self, path):
+    def is_point(self):
         """
-        Get the marks in a meta file.
-
-        Parameters
-        ----------
-        path : path-like
-            The path of a meta file.
+        Check if the guide mark is a point.
 
         Returns
         -------
-        list of dicts, [
-            {
-            position: double, 
-            length: double, 
-            text: str, 
-            group_id: int,
-            group_name: str
-            },
-            .
-            .
-            .
-            ]
-            A list of dictionaries. Each dictionary contains the start
-            position, length, text, group_id and group_name.
+        bool : True or False
+            `True` if the length is greater than 0. Otherwise, it returns `False`.
         """
-        with open(path, 'rb') as metafile:
-            content = metafile.read()
-        list_ = []
-        for mark in self._parse(content):
-            dict_ = {
-                'position': mark[0],
-                'length': mark[1],
-                'text': mark[2],
-                'group_id': mark[3],
-                'group_name': mark[4],
-            }
-            list_.append(dict_)
-        return list_
+        return self.length == 0
