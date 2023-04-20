4330dc5704c1f539d48020b85e58da40ec34726f
rolandomunoz <rolando.muar@gmail.com>
2023-04-20 00:26:46 -0500

    changed: import MarksGroup class

--- src/sis_meta/__init__.py
+++ src/sis_meta/__init__.py
@@ -1,3 +1,4 @@
 from sis_meta.meta import Meta
-from sis_meta.io.read import read_from_file
-from sis_meta.io.write import write_meta_file
+from sis_meta.marks_group import MarksGroup
+#from sis_meta.io.read import read_from_file
+#from sis_meta.io.write import write_meta_file
