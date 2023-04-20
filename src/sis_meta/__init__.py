21bf0e3daca614134f1cf6b8887e144bdfa3fd9e
rolandomunoz <rolando.muar@gmail.com>
2023-04-20 06:48:51 -0500

    edit: Update module names

--- src/sis_meta/__init__.py
+++ src/sis_meta/__init__.py
@@ -1,4 +1,4 @@
 from sis_meta.meta import Meta
-from sis_meta.marks_group import MarksGroup
+from sis_meta.groups_segment import GroupsSegment
 #from sis_meta.io.read import read_from_file
 #from sis_meta.io.write import write_meta_file
