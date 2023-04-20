7d2112ba097429da2e16a47062891de42d3de9b2
rolandomunoz <rolando.muar@gmail.com>
2023-04-20 00:24:12 -0500

    refactor: internal data structure is now a list of dicts. doc: more documentation

--- src/sis_meta/marks_group.py
+++ src/sis_meta/marks_group.py
@@ -4,6 +4,7 @@ Class for handling MARKS_GROUP in meta file.
 import re
 import io
 from importlib.resources import files
+from pprint import pprint
 
 from sis_meta.io._parse_groups_segment import discover_grouptree
 
@@ -27,7 +28,7 @@ class MarksGroup:
         if path is None:
             path = files('sis_meta.io.templates') / 'grouptree.meta'
             with open(path, 'rb') as raw_file:
-                self.data = discover_grouptree(raw_file, {})
+                self.data = discover_grouptree(raw_file, [])
         else:
             with open(path, 'rb') as raw_file:
                 match = GROUPS_SEGMENT.match(raw_file.read())
@@ -35,54 +36,47 @@ class MarksGroup:
                     raise IOError(f'{path} is not a meta file.')
                 self.data = discover_grouptree(
                     io.BytesIO(match.group(1)),
-                    {}
+                    []
                 )
         self.group_id = self._find_max_id(self.data)
 
     def _find_max_id(self, data):
         """
-        Find the max group id.
+        Find the max group id in a MARK_GROUP tree.
         """
-        for group_id, dict_ in data.items():
-            max_value = group_id
+        for dict_ in data:
+            max_value = dict_['id']
             if 'child' in dict_:
                 current_value = self._find_max_id(dict_['child'])
                 max_value = current_value if current_value > max_value else max_value
         return max_value
 
-    def _find_element(self, tree, path):
+    def _find_mark_group(self, data, path):
         """
-        Find an element within a tree by name attribute.
+        Find a MARK_GROUP dict.
         """
-        name = path[:1]
-        path_ = path[1:]
+        mark_group_name = path[0]
+        mark_group_name_path = path[1:]
 
         # Base case
-        if len(name) == 0: # Each element in the path has been consumed
-            return tree
+        if len(mark_group_name_path) == 0: # Each element in the path has been consumed
+            for dict_ in data:
+                if dict_['name'] == mark_group_name:
+                    return dict_
+            return None
 
         # Recursive case
-        # Given a path, find its nested dict
-        name_str = name[0]
-        for _, dict_ in tree.items():
-            if not dict_['name'] == name_str:
+        for dict_ in data:
+            if not dict_['name'] == mark_group_name:
                 continue
 
-            child = dict_.get('child')
-            if child is None:
-                if len(path_) == 0: # The elemt
-                    return self._find_element(dict_, path_)
-            else:
-                return self._find_element(child, path_)
-
-        raise MarkGroupNotFoundError(
-            f'mark group "{name[0]}" does not exist.'
-        )
+            if 'child' in dict_:
+                return self._find_mark_group(dict_['child'], mark_group_name_path)
 
     @staticmethod
     def _norm_path(path):
         """
-        Normalize path.
+        Normalize a MARK_GROUP path.
 
         Parameters
         ----------
@@ -98,20 +92,37 @@ class MarksGroup:
             path = 'All marks' if path == '' else f'All marks/{path}'
         return path
 
+    def is_path(self, path):
+        """
+        Check if the MARK_GROUP path exists.
+
+        Parameter
+        ---------
+        path : str
+            The path of a MARK_GROUP.
+
+        Returns
+        -------
+        bool
+            Return `True` if the path exists; otherwise, `False`.
+        """
+        path_list = self._norm_path(path).split('/')
+        path_exists = self._find_mark_group(self.data, path_list)
+
+        if path_exists is None:
+            return False
+        return True
+
     def insert(self, path, new_name, color=0, hotkey= 0, userparam=0):
         """
         Insert a mark group.
 
         Parameters
         ----------
-        tree : dict of dict
-            The MARKS_GROUP structure.
         path : str
-            The mark groups that contain the target mark group. When
-            a target mark group in nested inside more than one group,
-            use forward slashes (`/`) between the subgroups' names.
+            The path of the MARK_GROUP.
         new_name : str
-            The name of the new mark group
+            The name of the new mark group.
         color : int, default 0
             A number representing the color. Unknown yet.
         hotkey : int, default 0
@@ -134,23 +145,27 @@ class MarksGroup:
         path = self._norm_path(path)
         parent_names = path.split('/')
 
-        tree = self._find_element(self.data, parent_names)
+        mark_group = self._find_mark_group(self.data, parent_names)
+        if mark_group is None:
+            raise MarkGroupNotFoundError(f'Cannot found {path} path.')
+
+        dict_ = {
+            'id': self.group_id,
+            'name': new_name,
+            'color': str(color),
+            'hotkey': str(hotkey),
+            'userparam': str(userparam),
+        }
         self.group_id += 1
-        tree[self.group_id] = {
-                        'name': new_name,
-                        'color': str(color),
-                        'hotkey': str(hotkey),
-                        'userparam': str(userparam),
-                    }
+        mark_group.setdefault('child', [])
+        mark_group['child'].append(dict_)
 
     def update(self, path, name= None, color= None, hotkey= None, userparam= None):
         """
-        Update a mark group.
+        Update the attributes of a MARK_GROUP.
 
         path : str
-            The mark groups that contain the target mark group. When
-            a target mark group in nested inside more than one group,
-            use forward slashes (`/`) between the subgroups' names.
+            The path of the MARK_GROUP.
         name : str, defaul `None`
             The name of the new mark group
         color : int, default `None`
@@ -164,49 +179,48 @@ class MarksGroup:
         path = self._norm_path(path)
         path_list = path.split('/')
 
-        tree = self._find_element(self.data, path_list)
+        mark_group = self._find_mark_group(self.data, path_list)
+        if mark_group is None:
+            raise MarkGroupNotFoundError(f'Cannot found {path} path.')
 
         if not name is None:
-            tree['name'] = name
+            mark_group['name'] = name
 
         if not color is None:
-            tree['color'] = color
+            mark_group['color'] = color
 
         if not hotkey is None:
-            tree['hotkey'] = hotkey
+            mark_group['hotkey'] = hotkey
 
         if not userparam is None:
-            tree['userparam'] = userparam
+            mark_group['userparam'] = userparam
 
     def remove(self, path):
         """
-        Remove a mark group item.
+        Remove a MARK_GROUP.
 
         Parameters
         ----------
         path : str
-            The mark groups that contain the target mark group. When
-            a target mark group in nested inside more than one group,
-            use forward slashes (`/`) between the subgroups' names.
+            The path of the MARK_GROUP.
         """
-        path = self._norm_path(path)
-        path_list = path.split('/')
+        if not self.is_path(path):
+            raise MarkGroupNotFoundError(f'Cannot found {path} path.')
 
-        name = path_list[-1]
-        parents = path_list[:-1]
+        path_list = self._norm_path(path).split('/')
+        mark_group_name = path_list[-1]
+        mark_group_path = path_list[:-1]
+        parent_mark_group = self._find_mark_group(self.data, mark_group_path)
 
-        tree = self._find_element(self.data, parents)
-        group_id = None
-        for group_id_, dict_ in tree.items():
-            if dict_['name'] == name:
-                group_id = group_id_
+        sub_marks_group = parent_mark_group['child']
+        for index_, dict_ in enumerate(sub_marks_group):
+            if dict_['name'] == mark_group_name:
+                index = index_
                 break
+        sub_marks_group.pop(index)
 
-        if group_id is None:
-            raise MarkGroupNotFoundError(
-                f'mark group "{name}" does not exist'
-            )
-        tree.pop(group_id)
+        if len(sub_marks_group) == 0:
+            parent_mark_group.pop('child')
 
 class MarkGroupNotFoundError(Exception):
     """
