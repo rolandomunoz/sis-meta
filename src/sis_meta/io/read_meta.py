5a41d4968be0b0fd3c2030b290ed6519a0c7a83f
rolandomunoz <rolando.muar@gmail.com>
2023-04-22 17:33:37 -0500

    changed: read_from_file returns a Meta object

--- src/sis_meta/io/read_meta.py
+++ src/sis_meta/io/read_meta.py
@@ -1,3 +1,12 @@
+"""
+Read a meta file.
+"""
+from sis_meta import Meta
+from sis_meta.groups import GroupsSegment
+from sis_meta.marks import GuideMark
+from sis_meta.io._parse_marks_segment import parse_marks_segment
+from sis_meta.io.exceptions import MetaFileError
+from sis_meta.regex import META_PATTERN
 
 def read_from_file(path):
     """
@@ -13,6 +22,27 @@ def read_from_file(path):
     :class:`sis_meta.Meta`
         An object that contains marks.
     """
-    meta_obj = meta.Meta()
-    meta_obj.data = _parse_file(path)
-    return meta_obj
+    with open(path, 'rb') as raw_file:
+        raw_data = raw_file.read()
+
+    match = META_PATTERN.match(raw_data)
+    if not match:
+        MetaFileError(f'Not a Meta File: {path}')
+
+    meta = Meta() # Initialize a meta object
+
+    # GROUPS_SEGMENT
+    meta._groups = GroupsSegment(path)
+
+    # MARKS_SEGMENT
+    marks_segment = parse_marks_segment(
+        match.group('MARKS_SEGMENT')
+    )
+    list_ = []
+    for position, length, text, group_id in marks_segment:
+        group_name = meta._groups.get_name(group_id)
+        list_.append(
+            GuideMark(position, length, text, group_id, group_name)
+        )
+    meta._data = list_
+    return meta
