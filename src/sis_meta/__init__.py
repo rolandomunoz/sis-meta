3a6f533542ec866b6c9d049c5274c4bfa49302e6
rolandomunoz <rolando.muar@gmail.com>
2023-04-22 22:32:28 -0500

    fix: avoid circular importing

--- src/sis_meta/__init__.py
+++ src/sis_meta/__init__.py
@@ -1,4 +1,2 @@
-from sis_meta.meta import Meta
-from sis_meta.groups import GroupsSegment
+from sis_meta.meta.meta import Meta
 from sis_meta.io.read_meta import read_from_file
-#from sis_meta.io.write import write_meta_file
