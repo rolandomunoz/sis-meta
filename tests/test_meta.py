9f5185f8e684b284e0bf683e93b44b88ea895f92
rolandomunoz <rolando.muar@gmail.com>
2023-04-23 21:31:38 -0500

    feat: add new methods

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -5,13 +5,26 @@ from sis_meta import Meta
 meta = Meta()
 
 # Manage groups
-groups = meta.manage_groups()
-groups.insert('TextGrid')
-groups.insert('Rolando', 'TextGrid')
-groups.insert('Aarón', 'TextGrid')
+meta.insert_group('TextGrid')
+meta.insert_group('TextGrid/Rolando')
+meta.insert_group('TextGrid/Aarón')
+meta.insert_group('TextGrid/Javier')
 
 # Insert marks
-meta.insert_guide_mark('TextGrid/Rolando', 1.753, 2.242, 'Aló')
+meta.insert_guide_mark('TextGrid/Javier', 1.753, 2.242, 'Aló')
 meta.insert_guide_mark('TextGrid/Aarón', 3.2424, 2.853, 'hola')
 meta.insert_guide_mark('TextGrid/Rolando', 5.753, 3.242, 'Estaba pensando en ir')
 meta.insert_guide_mark('TextGrid/Aarón', 8.753, 10.242, 'Ahm...')
+
+meta.update_group('TextGrid/Rolando', 'Rolo')
+meta.update_group('TextGrid/Javier', 'Javi')
+
+meta.remove_group('TextGrid/Javi')
+meta.remove_group('TextGrid/Rolo')
+meta.remove_group('TextGrid/Aarón')
+
+for mark in meta:
+    print(mark)
+
+print(len(meta))
+meta.write('holi.meta')
